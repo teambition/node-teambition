0b3c0aed22b00b6a61bc2790de5235db237d25fd
sunnoder <m18205253820@163.com>
2017-03-23 23:20:13 +0800

    refactor/support-promise

--- README.md
+++ README.md
@@ -1,25 +1,61 @@
-# node-teambition
+```bash
+__                __                               __      _    __     _                
+____   ____   ____/ /  ___          / /_  ___   ____ _   ____ ___    / /_    (_)  / /_   (_)  ____    ____
+/ __ \ / __ \ / __  /  / _ \ ______ / __/ / _ \ / __ `/  / __ `__ \  / __ \  / /  / __/  / /  / __ \  / __ \
+/ / / // /_/ // /_/ /  /  __//_____// /_  /  __// /_/ /  / / / / / / / /_/ / / /  / /_   / /  / /_/ / / / / /
+/_/ /_/ \____/ \__,_/   \___/        \__/  \___/ \__,_/  /_/ /_/ /_/ /_.___/ /_/   \__/  /_/   \____/ /_/ /_/
+
+
+```
 node sdk for teambition
 
-## 安装
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+## Installation
 ```
-npm i teambition
+yarn add teambition
 ```
 
-## 参数说明
+## Options
+Initialize teambition plugin with the given options.
 
+```JavaScript
 Teambition(access_token, { host: "", authHost: "", protocol: "" })
-
-- access_token: 用户通过Teambition Account授权获取到的token, 用于验证请求是否合法并经过用户授权
-- host: `非必需参数`, 指定API地址
-- authHost: `非必需参数`, 指定的认证host地址
-
-
-## 使用
 ```
-Teambition = require('teambition')
-teambition = new Teambition('access_token')
+Options:
 
-teambition.api '/users/me', (err, user) ->
-  # 获取到个人信息
+ - `access_token` 用户通过Teambition Account授权获取到的token, 用于验证请求是否合法并经过用户授权
+ - `host` 非必需参数, 指定API地址s
+ - `authHost` 非必需参数, 指定的认证host地址
+
+
+## Usage
+
+* callback
+```JavaScript
+let accessToken = 'teambition accessToken'
+let teambition = new Teambition(accessToken)
+
+teambition.get('/users/me', (err, data) => {
+  // user's profile
+  if (err) throw err
+  console.log(data)
+})
+
 ```
+
+* promise
+```JavaScript
+let accessToken = 'teambition accessToken'
+let teambition = new Teambition(accessToken)
+teambition
+  .get('/users/me')
+  .then(userprofile => console.log(userprofile))
+  .catch(err => console.log(err))
+```
+
+## License
+
+ISC License
+
+Copyright (c) 2017 Teambition
---
+++ test/index.test.js
@@ -0,0 +1,159 @@
+const Teambition = require('../lib/index')
+const should = require('should')
+const expect = require('expect')
+const nock = require('nock')(`http://localhost:3000`)
+
+let accessToken = 'teambition accessToken'
+let config = {
+  protocol: 'http',
+  host: 'localhost:3000/api',
+  authHost: 'localhost:3000/authHost'
+}
+let teambition = new Teambition(accessToken, config)
+describe('Teambition SDK Testing', () => {
+  before(() => {
+    nock
+            .get('/api/users/me')
+            .reply(200, 'ok')
+            .get('/api/users/me')
+            .reply(200, 'ok')
+            .post('/api/users/me', {name: 'tbUser'})
+            .reply(200, 'ok')
+            .post('/api/users/me', {name: 'tbUser'})
+            .reply(200, 'ok')
+            .put('/api/users/me', {name: 'tbUser'})
+            .reply(200, 'ok')
+            .put('/api/users/me', {name: 'tbUser'})
+            .reply(200, 'ok')
+            .delete('/api/users/me', {name: 'tbUser'})
+            .reply(200, 'ok')
+            .delete('/api/users/me', {name: 'tbUser'})
+            .reply(200, 'ok')
+            .delete('/api/users/me', {name: 'tbUser'})
+            .reply(500, 'failed')
+            .delete('/api/users/me', {name: 'tbUser'})
+            .reply(500, 'failed')
+  })
+
+  it('should resolve get user if valid promise request', (done) => {
+    teambition
+        .get('/users/me')
+        .then(userprofile => {
+          expect(userprofile).toBe('ok')
+          done()
+        })
+  })
+
+  it('should resolve get user if valid callback request', (done) => {
+    teambition.get('/users/me', (err, userprofile) => {
+        // user's profile
+      if (err) throw err
+      expect(userprofile).toBe('ok')
+      done()
+    })
+  })
+
+  it('should resolve post user if valid promise request', (done) => {
+    teambition
+        .post('/users/me', {
+          name: 'tbUser'
+        })
+        .then(userprofile => {
+          expect(userprofile).toBe('ok')
+          done()
+        })
+  })
+
+  it('should resolve post user if valid callback request', (done) => {
+    teambition.post('/users/me', {name: 'tbUser'}, (err, userprofile) => {
+        // user's profile
+      if (err) throw err
+      expect(userprofile).toBe('ok')
+      done()
+    })
+  })
+
+  it('should resolve put user if valid promise request', (done) => {
+    teambition
+        .put('/users/me', {
+          name: 'tbUser'
+        })
+        .then(userprofile => {
+          expect(userprofile).toBe('ok')
+          done()
+        })
+  })
+
+  it('should resolve put user if valid callback request', (done) => {
+    teambition.put('/users/me', {name: 'tbUser'}, (err, userprofile) => {
+        // user's profile
+      if (err) throw err
+      expect(userprofile).toBe('ok')
+      done()
+    })
+  })
+
+  it('should resolve delete user if valid promise request', (done) => {
+    teambition
+        .del('/users/me', {
+          name: 'tbUser',
+          headers: {
+            'x-credential': 'jwt'
+          }
+        })
+        .then(userprofile => {
+          expect(userprofile).toBe('ok')
+          done()
+        })
+  })
+
+  it('should resolve delete user if valid callback request', (done) => {
+    teambition.del('/users/me', {name: 'tbUser'}, (err, userprofile) => {
+        // user's profile
+      if (err) throw err
+      expect(userprofile).toBe('ok')
+      done()
+    })
+  })
+
+  it('should reject delete user if valid promise request', (done) => {
+    teambition
+        .del('/users/me', {
+          name: 'tbUser',
+          headers: {
+            'x-credential': 'jwt'
+          }
+        })
+        .then(userprofile => {})
+        .catch(err => {
+          expect(err).toBe('failed')
+          done()
+        })
+  })
+
+  it('should reject delete user if valid callback request', (done) => {
+    teambition.del('/users/me', {name: 'tbUser'}, (err, userprofile) => {
+        // user's profile
+      expect(err).toBe('failed')
+      done()
+    })
+  })
+
+  it('should resolve getAuthorizeUrl if valid request', (done) => {
+    let auth = teambition.getAuthorizeUrl('tb-01', '/test', 'true')
+    should.exist(auth)
+    done()
+  })
+
+  it('should resolve getAccessTokenUrl if valid request', (done) => {
+    let accessTokenUrl = teambition.getAccessTokenUrl()
+    should.exist(accessTokenUrl)
+    done()
+  })
+
+  it('should resolve getAccessTokenUrl if valid request', (done) => {
+    let authCallback = teambition.authCallback('tb-01', 'teambition')
+    should.exist(authCallback)
+    done()
+  })
+})
